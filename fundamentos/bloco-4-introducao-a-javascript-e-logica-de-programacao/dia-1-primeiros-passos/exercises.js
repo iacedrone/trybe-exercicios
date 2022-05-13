0dc03c57200549c2df1dba08db997403db238454
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-13 10:57:27 -0300

    4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
@@ -1,11 +1,9 @@
-const a = 9;
-const b = 4;
-const c = 3;
+const value = 0;
 
-if (a > b && a > c) {
-    console.log(a)
-} else if (b > a && b > c) {
-    console.log(b)
-} else if (c > a && c > b) {
-    console.log(c)
+if (value > 0) {
+    console.log('positive')
+} else if (value < 0) {
+    console.log('negative')
+} else {
+    console.log('zero')
 }
