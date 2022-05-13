0077a13df1f2157759af6dd402d4c6fc06e69947
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-13 10:29:23 -0300

    2. Crie uma estrutura condicional com o switch/case que irá imprimir as mensagens do exercício anterior se o estado da pessoa candidata for 'aprovada', 'lista' ou 'reprovada'. Como default, imprima a mensagem de 'não se aplica'.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/parte5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/parte5.js
@@ -1,1 +1,15 @@
 let estado = 'aprovada';
+
+switch(estado) {
+    case 'aprovada':
+        console.log(estado)
+        break
+    case 'reprovada':
+        console.log(estado)
+        break
+    case 'lista':
+        console.log(estado)
+        break
+    default:
+        console.log('não se aplica')
+}
