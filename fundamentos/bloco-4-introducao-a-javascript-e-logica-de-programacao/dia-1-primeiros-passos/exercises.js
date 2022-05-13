fa5bb212a4888eb480ca7f198c72e3e74c0fdb53
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-13 13:13:05 -0300

    8. Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
@@ -1,17 +1,9 @@
-let nota = -1;
+const a = 1;
+const b = 1;
+const c = 1;
 
-if (nota < 0 || nota > 100) {
-    console.log('ERRO')
-} else if (nota >= 90) {
-    console.log('A')
-} else if (nota >= 80) {
-    console.log('B')
-} else if (nota >= 70) {
-    console.log('C')
-} else if (nota >= 60) {
-    console.log('D')
-} else if (nota >= 50) {
-    console.log('E')
-} else if (nota < 50) {
-    console.log('F')
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    console.log(true)
+} else {
+    console.log(false)
 }
