38ce1c0d4065fb57bea3085b4d4435dd614ae5ad
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-13 12:57:52 -0300

    7. Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
@@ -1,18 +1,17 @@
-let peca = 'Rei';
-peca = peca.toLowerCase()
+let nota = -1;
 
-if (peca === 'rei') {
-    console.log('Qualquer direção')
-} else if (peca === 'rainha') {
-    console.log('Qualquer direção')
-} else if (peca === 'torre') {
-    console.log('Linha reta')
-} else if (peca === 'bispo') {
-    console.log('Diagonal')
-} else if (peca === 'cavalo') {
-    console.log('Se move em L')
-} else if (peca === 'peão') {
-    console.log('Para a frente e para diagonal')
-} else {
+if (nota < 0 || nota > 100) {
     console.log('ERRO')
+} else if (nota >= 90) {
+    console.log('A')
+} else if (nota >= 80) {
+    console.log('B')
+} else if (nota >= 70) {
+    console.log('C')
+} else if (nota >= 60) {
+    console.log('D')
+} else if (nota >= 50) {
+    console.log('E')
+} else if (nota < 50) {
+    console.log('F')
 }
