38acaa11e50b9127ff1f45bb391947390f6426e9
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-13 10:54:24 -0300

    3. Faça um programa que retorne o maior de três números.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercises.js
@@ -1,11 +1,11 @@
-const a = 8;
-const b = 9;
+const a = 9;
+const b = 4;
+const c = 3;
 
-if (a > b) {
+if (a > b && a > c) {
     console.log(a)
-} else if (b > a) {
+} else if (b > a && b > c) {
     console.log(b)
-} else {
-    console.log('Os números são iguais')
+} else if (c > a && c > b) {
+    console.log(c)
 }
-
