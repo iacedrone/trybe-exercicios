1973580af30c7a6d91f984208ca0549297b54a91
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-16 19:20:19 -0300

    3. Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercises.js
@@ -1,6 +1,17 @@
-let word = 'tryber';
-let palavraInvertida = [];
-for (index = word.length - 1; index >= 0; index -= 1) {
-    palavraInvertida.push(word[index])
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0]
+let menorPalavra = array[0]
+
+for (let iMaior = 0; iMaior < array.length; iMaior += 1) {
+    if (array[iMaior].length > maiorPalavra.length) {
+        maiorPalavra = array[iMaior]
+    }
 }
-console.log(palavraInvertida)
+
+for (let iMenor = 0; iMenor < array.length; iMenor += 1) {
+    if (array[iMenor].length < menorPalavra.length) {
+        menorPalavra = array[iMenor]
+    }
+}
+
+console.log(menorPalavra);
