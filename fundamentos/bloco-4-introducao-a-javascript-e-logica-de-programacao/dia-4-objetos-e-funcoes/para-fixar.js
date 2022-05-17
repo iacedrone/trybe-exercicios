3143e0f8ec905f8093ffbc96dbaa092c6d4664b1
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-17 16:20:24 -0300

    3. Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/para-fixar.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/para-fixar.js
@@ -2,12 +2,14 @@ let name = 'Marta';
 let lastName = 'Silva';
 let age = 34;
 let medals = { golden: 2, silver: 3 };
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
 
 let player = {
     name,
     lastName,
     age,
-    medals
+    medals,
+    bestInTheWorld
 }
 
 console.log('A jogadora '+ player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade ');
