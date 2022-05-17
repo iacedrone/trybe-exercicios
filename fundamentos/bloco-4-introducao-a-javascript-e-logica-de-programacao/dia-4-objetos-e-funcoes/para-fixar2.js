1cac8f2d210dfdc65a3daf741f0dbb1de2a5fb8b
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-17 18:45:36 -0300

    2. utilize for/in e imprima um console.log com as chaves e valores do objeto.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/para-fixar2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/para-fixar2.js
@@ -1,9 +1,9 @@
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge',
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
 };
 
-for (name in names) {
-    console.log('Olá ' + names[name])
+for (key in car) {
+    console.log(key, car[key])
 }
