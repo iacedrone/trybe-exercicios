07b430478693eb8d6fa975e1c51fb331d009bdcc
Chrystian Lunetta <cchrystianavaete@gmail.com>
2022-05-17 19:46:37 -0300

    Usando o objeto abaixo, faça os exercícios a seguir:

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/parte1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/parte1.js
@@ -1,21 +1,12 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-};
-
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-}
-
-for (key in info) {
-    if (info[key] === info2[key]) {
-        console.log('Ambos recorrentes');
-    } else {
-        console.log(info[key] + ' // ' + info2[key]);
-    }
-}
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
