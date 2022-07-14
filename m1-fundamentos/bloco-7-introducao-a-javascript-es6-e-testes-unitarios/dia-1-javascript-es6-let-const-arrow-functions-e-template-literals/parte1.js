2eef8cede445179a236aeda2fab43f18485e1914
iacedrone <cchrystianavaete@gmail.com>
2022-07-14 18:55:47 -0300

    2. Crie uma função que retorne um array em ordem crescente.

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
@@ -1,5 +1,15 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const sortOddsAndEvens = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+    return oddsAndEvens;
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
