9a7707336474ed6c225bf33954a259bf67daeb6d
iacedrone <cchrystianavaete@gmail.com>
2022-07-14 18:42:44 -0300

    Questão 2

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
@@ -1,12 +1,5 @@
-const testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-  }
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-  testingScope(true);
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
