fba204622e5902a76da355939d9c5da0bdc7fd00
iacedrone <cchrystianavaete@gmail.com>
2022-07-18 19:36:56 -0300

    2- 1. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -1,14 +1,5 @@
-const sum = require('./agora-a-pratica');
+const myRemove = require('./agora-a-pratica.js')
 
-test('O retorno de sum(4, 5) é 9?', () => {
-    expect(sum(4,5)).toBe(9)
-})
-test('O retorno de sum(0, 0) é 0?', () => {
-    expect(sum(0,0)).toBe(0)
-})
-test('A função sum lança um erro quando os parâmetros são 4 e "5"?', () => {
-    expect(() => sum(4,"5")).toThrowError()
-})
-test('A mensagem de erro é "parameters must be numbers" quando realiza a chamada sum(4, "5")?', () => {
-    expect(() => sum(4, "5")).toThrowError('parameters must be numbers')
+test('A chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado?', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
 })
