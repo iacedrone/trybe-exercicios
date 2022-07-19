e589f0b79090159b4b65890201ee8df412703ce7
iacedrone <cchrystianavaete@gmail.com>
2022-07-19 19:51:14 -0300

    4- 2. Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente / 4- 3. Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.js
@@ -1,9 +1,21 @@
-const encode = (a, b) => {
-  return(a + b);
+const encode = (string) => {
+  let newString = string.toLowerCase().replaceAll('a', '1')
+  newString = newString.replaceAll('e', '2')
+  newString = newString.replaceAll('i', 3)
+  newString = newString.replaceAll('o', 4)
+  newString = newString.replaceAll('u', 5)
+
+  return(newString)
 }
 
-const decode = () => {
+const decode = (number) => {
+  let newNumber = number.toString().replaceAll('1', 'a')
+  newNumber = newNumber.replaceAll('2', 'e')
+  newNumber = newNumber.replaceAll('3', 'i')
+  newNumber = newNumber.replaceAll('4', 'o')
+  newNumber = newNumber.replaceAll('5', 'u')
 
+  return(newNumber)
 }
 
 module.exports = {encode, decode};
--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -1,6 +1,16 @@
 const {encode, decode} = require('./agora-a-pratica.js');
 
-test('Encode e decode são funções?', () => {
-    expect(typeof encode).toBe('function')
-    expect(typeof decode).toBe('function')
+describe('Questão 1', () => {
+    test('Encode e decode são funções?', () => {
+        expect(typeof encode).toBe('function')
+        expect(typeof decode).toBe('function')
+    })
+})
+
+test('2. Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+    expect(encode('aeiou')).toBe('12345')
+})
+
+test('3. Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
+    expect(decode(12345)).toBe('aeiou')
 })
