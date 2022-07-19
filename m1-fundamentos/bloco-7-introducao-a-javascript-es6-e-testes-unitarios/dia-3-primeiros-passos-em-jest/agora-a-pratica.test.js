cfbe5249be2fd6192d9c0167b23469d23b6195fa
iacedrone <cchrystianavaete@gmail.com>
2022-07-18 23:56:50 -0300

    3- 1. Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -1,11 +1,5 @@
-const myRemove = require('./agora-a-pratica.js')
+const myFizzBuzz = require('./agora-a-pratica.js');
 
-test('A chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado?', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
-})
-test('A chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
-})
-test('a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado?', () => {
-    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+test('Verifica se a função myFizzBuzz retorna o resultado esperado.', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz')
 })
