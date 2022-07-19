9ac120986e5bc48a87188bd12291d38569319188
iacedrone <cchrystianavaete@gmail.com>
2022-07-19 00:19:16 -0300

    4- 1. Teste se encode e decode são funções

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.js
@@ -1,9 +1,9 @@
-function myFizzBuzz(num) {
-  if (typeof num !== 'number') return false;
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
-  return num;
+const encode = (a, b) => {
+  return(a + b);
 }
 
-module.exports = myFizzBuzz;
+const decode = () => {
+
+}
+
+module.exports = {encode, decode};
--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -1,19 +1,5 @@
-const myFizzBuzz = require('./agora-a-pratica.js');
+const {encode, decode} = require('./agora-a-pratica.js');
 
-test('Verifica se a função myFizzBuzz retorna o resultado esperado.', () => {
-    expect(myFizzBuzz(15)).toBe('fizzbuzz')
-})
-
-test('Faça uma chamada com um número divisível por 3, outro por 5 e verifique se os retornos são os esperados', () => {
-    expect(myFizzBuzz(3)).toBe('fizz')
-    expect(myFizzBuzz(5)).toBe('buzz')
-})
-
-describe('Questões 4 e 5', () => {
-    test('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
-        expect(myFizzBuzz(7)).toBe(7)
-    })
-    test('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
-        expect(myFizzBuzz('Muito doido')).toBe(false)
-    })
+test('Encode e decode são funções?', () => {
+    expect(typeof encode).toBe('function')
 })
