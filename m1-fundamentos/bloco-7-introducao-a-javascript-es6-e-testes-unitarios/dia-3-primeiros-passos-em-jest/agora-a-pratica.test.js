9c9c17927fdde208dd536f60d50c8b3040d3ece2
iacedrone <cchrystianavaete@gmail.com>
2022-07-18 23:12:34 -0300

    2- 3. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -6,3 +6,6 @@ test('A chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado?', () => {
 test('A chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
 })
+test('a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado?', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+})
