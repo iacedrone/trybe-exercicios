7fa07b95d01cad26468c48b7a8ddd7dc11688764
iacedrone <cchrystianavaete@gmail.com>
2022-07-19 00:00:31 -0300

    3- 3. Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -6,4 +6,5 @@ test('Verifica se a função myFizzBuzz retorna o resultado esperado.', () => {
 
 test('Faça uma chamada com um número divisível por 3, outro por 5 e verifique se os retornos são os esperados', () => {
     expect(myFizzBuzz(3)).toBe('fizz')
+    expect(myFizzBuzz(5)).toBe('buzz')
 })
