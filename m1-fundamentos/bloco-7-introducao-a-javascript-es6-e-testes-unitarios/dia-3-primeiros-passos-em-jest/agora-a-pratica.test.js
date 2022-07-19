f8c5a1da48ddc1b4df72c09fc22acd0e60ad239e
iacedrone <cchrystianavaete@gmail.com>
2022-07-19 00:20:02 -0300

    4- 1. Teste se encode e decode são funções

--- m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
+++ m1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/agora-a-pratica.test.js
@@ -2,4 +2,5 @@ const {encode, decode} = require('./agora-a-pratica.js');
 
 test('Encode e decode são funções?', () => {
     expect(typeof encode).toBe('function')
+    expect(typeof decode).toBe('function')
 })
